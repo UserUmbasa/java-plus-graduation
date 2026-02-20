cc02ad1f8ce431ca62114d79742aa73ee4f3c395
Umbasa <butuzov_tema@yahoo.com>
2026-02-20 08:56:45 +0300

    закончил Analyzer

---
+++ stats/analyzer/src/main/java/ru/practicum/analyzer/controller/RecommendationsController.java
@@ -0,0 +1,72 @@
+package ru.practicum.analyzer.controller;
+
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import ru.practicum.analyzer.handler.RecommendationsHandler;
+import ru.practicum.ewm.stats.proto.InteractionsCountRequestProto;
+import ru.practicum.ewm.stats.proto.RecommendedEventProto;
+import ru.practicum.ewm.stats.proto.SimilarEventsRequestProto;
+import ru.practicum.ewm.stats.proto.UserPredictionsRequestProto;
+import ru.yandex.practicum.ewm.stats.services.RecommendationsControllerGrpc;
+
+@Slf4j
+@GrpcService
+@RequiredArgsConstructor
+public class RecommendationsController extends RecommendationsControllerGrpc.RecommendationsControllerImplBase {
+    private final RecommendationsHandler handler;
+
+    @Override
+    public void getRecommendationsForUser(UserPredictionsRequestProto request,
+                                          StreamObserver<RecommendedEventProto> responseObserver) {
+        try {
+            log.info("Запрос получения рекомендаций : {}", request);
+            handler.getRecommendationsForUser(request).forEach(responseObserver::onNext);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            log.error("Ошибка получения рекомендаций : {}", e.getMessage(), e);
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
+    }
+
+    @Override
+    public void getSimilarEvents(SimilarEventsRequestProto request,
+                                 StreamObserver<RecommendedEventProto> responseObserver) {
+        try {
+            log.info("Запрос получения  похожих мероприятий: {}", request);
+            handler.getSimilarEvents(request).forEach(responseObserver::onNext);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            log.error("Ошибка получения  похожих мероприятий: {}", e.getMessage(), e);
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
+    }
+
+    @Override
+    public void getInteractionsCount(InteractionsCountRequestProto request,
+                                     StreamObserver<RecommendedEventProto> responseObserver) {
+        try {
+            log.info("Запрос суммы весов для события: {}", request);
+            handler.getInteractionsCount(request).forEach(responseObserver::onNext);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            log.error("Ошибка суммы весов для событий: {}", e.getMessage(), e);
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
+    }
+}
